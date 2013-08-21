d5e120ef87d4d4d939034eebf07439554e61be5b
Gadi Cohen <dragon@wastelands.net>
2013-08-21 17:05:12 +0300

    more livedoc improvements

--- examples/messageformat/messageformat.js
+++ examples/messageformat/messageformat.js
@@ -1,10 +1,46 @@
 if (Meteor.isClient) {
 
+  Template.main.getName = function() {
+    var lang = Session.get('lang');
+    return lang == 'he' ? 'גדי' : 'Gadi';
+  }
+
+  Session.setDefault('NUM', 1);
+  Template.main.getNum = function() {
+    return Session.get('NUM');
+  }
+
+  function myTrim(text, indent) {
+    text = text.replace(/^\n*/, '').replace(/\t/g, '  ');
+    var re = /^ */;
+    var origIndent = re.exec(text)[0];
+    re = new RegExp('^' + origIndent, 'gm');
+    return text.replace(re, indent).replace(/\s*$/, '');
+  }
+
   Template.main.example = function(key, message, params) {
-    console.log(params);
+    var js;
+    if (typeof key == "function") {
+        // if called as a block helper
+        message = myTrim(key.fn(this), '   ');
+        console.log(message)
+        params = { hash: key.hash };
+        console.log(params);
+        js = myTrim(key.inverse(this), '');
+        key = params.KEY;
+    } else {
+        console.log(params);
+        message = params ? message : null;
+        js = params.extra;
+        console.log(params);
+    }
     return new Handlebars.SafeString(Template.example({
-      key: key, message: message, params: params, paramOverride: params.hash.paramOverride, extra: params.hash.extra
+      key: key, message: message, params: params, js: js,
+      paramOverride: params.hash && params.hash.paramOverride
     }));
+  }
+  Template.example.blah = function() {
+    console.log(this);
   }
 
   Template.example.paramsStr = function() {
@@ -17,11 +53,6 @@ if (Meteor.isClient) {
 
   Template.example.longMessage = function() {
     return this.message.length > 20;
-  }
-
-  Session.setDefault('NUM', 1);
-  Template.main.NUM = function() {
-    return Session.get('NUM');
   }
 
   Session.setDefault('lang', 'en');
