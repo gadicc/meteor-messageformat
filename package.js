db661a4120c16b60df4ad73eba915fa6600b11e6
Gadi Cohen <dragon@wastelands.net>
2013-11-18 20:16:35 +0200

    make available on the server too

--- package.js
+++ package.js
@@ -5,8 +5,8 @@ Package.describe({
 Package.on_use(function (api) {
 	api.use('headers', ['client', 'server']);
 	api.use('handlebars', 'client');
-    api.add_files('lib/messageformat.js/messageformat.js', 'client');
-    api.add_files(['messageformat.js', 'locale-all.js'], 'client');  
-    if (api.export)
-    	api.export(['MessageFormatCache', 'mf'], 'client');
+	api.add_files('lib/messageformat.js/messageformat.js', ['client', 'server']);
+	api.add_files(['messageformat.js', 'locale-all.js'], ['client', 'server']);  
+	if (api.export)
+		api.export(['MessageFormatCache', 'mf'], ['client', 'server']);
 });
