79f98b736d7ddaa5c3ff6b701d3eecc8ef4c1c09
Gadi Cohen <dragon@wastelands.net>
2014-02-11 20:27:38 +0200

    switch to meteorite for package management in laika test

--- run_tests.sh
+++ run_tests.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-PACKAGE_NAME=messageformat
-PACKAGE_PATH=laika/packages/$PACKAGE_NAME
-
-#move and setup packages
-rm -rf $PACKAGE_PATH/lib
-rm -rf $PACKAGE_PATH/package.js
-mkdir -p $PACKAGE_PATH
-
-cp -rf ./lib $PACKAGE_PATH
-cp -rf ./package.js $PACKAGE_PATH
 
 cd laika
-laika -V -t 5000 $@
+if [ -n "$LAIKA_OPTIONS" ] ; then
+	laika $LAIKA_OPTIONS
+else
+	laika -V -t 5000 $@
+fi
+
