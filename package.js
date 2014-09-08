90542f652917d8aa8acdc5783f3f7571aa97a722
Gadi Cohen <dragon@wastelands.net>
2014-09-08 12:06:54 +0300

    v0.0.41; fix compat with 0.8.3, initial but broken laika 0.9 support (#140)

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name:    "gadicohen:messageformat",
-  version: "0.0.40",
+  version: "0.0.41",
   summary: "MessageFormat support, the Meteor way",
   git:      "https://github.com/gadicc/meteor-messageformat.git",
 });
--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
+echo "Testing on Meteor >= 0.9.0 (1/2)"
+echo "--------------------------------"
+echo
+
 cd laika
+
+echo "Test disabled until laika#140 is fixed..."
+echo
+echo
+
+#if [ -n "$LAIKA_OPTIONS" ] ; then
+#	laika $LAIKA_OPTIONS
+#else
+#	laika -V -t 5000 $@
+#fi
+
+
+echo "Testing on Meteor <= 0.8.3 (2/2)"
+echo "--------------------------------"
+echo
+
+cd ../laika-0.8.3
+
 if [ -n "$LAIKA_OPTIONS" ] ; then
 	laika $LAIKA_OPTIONS
 else
