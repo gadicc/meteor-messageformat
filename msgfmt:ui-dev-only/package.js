5ec9b3335f642d9f1ba9366d34d19a555dfcbec1
Daniel Speichert <daniel@speichert.pro>
2016-01-10 02:34:14 +0100

    Bump ui-dev-only to newest ui version

--- msgfmt:ui-dev-only/package.js
+++ msgfmt:ui-dev-only/package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name:      "msgfmt:ui-dev-only",
-  version:   "2.0.0-preview.1",
+  version:   "2.0.0-preview.2",
   summary:   "msgfmt-ui that is never deployed",
   git:       "https://github.com/gadicc/meteor-messageformat.git",
   debugOnly: true
 });
 
 Package.onUse(function (api) {
-  api.use('msgfmt:ui@2.0.0-preview.2');
+  api.use('msgfmt:ui@2.0.0-preview.6');
   api.imply('msgfmt:ui');
 });
